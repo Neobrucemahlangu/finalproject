cd2fcb6a0d768d7a081bc5b7803b0c35cbf51e37
Neobrucemahlangu <113622046+Neobrucemahlangu@users.noreply.github.com>
2025-07-22 23:21:35 +0000

    Setup Heroku deployment: backend start script and React build

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,10 @@
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
 
 const app = express();
 
-app.use(express.json());
-
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.send("API is running...");
 });
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../build", "index.html"));
-  });
-}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
